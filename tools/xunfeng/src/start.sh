076f097df01cd2ea8545279dd927e98bb52de383
Medicean <Medici.Yan@Gmail.com>
2016-12-20 21:23:11 +0800

    (Update Tools: XunFeng) 删除 mongo auth

--- tools/xunfeng/src/start.sh
+++ tools/xunfeng/src/start.sh
@@ -4,7 +4,10 @@ XUNFENG_LOG=/var/log/xunfeng
 
 mkdir -p ${XUNFENG_LOG}
 
-nohup /opt/mongodb/bin/mongod --port 65521 --dbpath=/opt/mongodb/data --auth  > ${XUNFENG_LOG}/db.log &
+nohup /opt/mongodb/bin/mongod --port 65521 --dbpath=/opt/mongodb/data > ${XUNFENG_LOG}/db.log &
+
+mongo 127.0.0.1:65521/xunfeng < /opt/mongodb/mongoauth
+
 nohup python ${XUNFENG_BASE}/Run.py > ${XUNFENG_LOG}/web.log &
 nohup python ${XUNFENG_BASE}/aider/Aider.py > ${XUNFENG_LOG}/aider.log &
 nohup python ${XUNFENG_BASE}/nascan/NAScan.py > ${XUNFENG_LOG}/scan.log &
